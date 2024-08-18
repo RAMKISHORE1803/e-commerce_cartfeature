09ac38fae3369e4ac91ef0cf16b56fe2711ba339
RamKishore <mramki1803@gmail.com>
2024-08-18 12:24:13 +0530

    fixed cart

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -2,6 +2,7 @@
 import { useSearchParams } from "next/navigation";
 import ProductCard from "../components/product/ProductCard";
 import useFetchProducts from "../hooks/useFetchProducts";
+import { Suspense } from "react";
 
 function Dashboard() {
   const searchParams = useSearchParams();
@@ -10,19 +11,21 @@ function Dashboard() {
 
   return (
     <div className="px-20">
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-        {products.map((product) => (
-          <ProductCard
-            key={product.id}
-            id={product.id}
-            name={product.name}
-            dimensions={product.dimensions}
-            reviews={product.reviews}
-            price={product.price}
-            images={product.images}
-          />
-        ))}
-      </div>
+      <Suspense fallback={<div>Loading...</div>}>
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+          {products.map((product) => (
+            <ProductCard
+              key={product.id}
+              id={product.id}
+              name={product.name}
+              dimensions={product.dimensions}
+              reviews={product.reviews}
+              price={product.price}
+              images={product.images}
+            />
+          ))}
+        </div>
+      </Suspense>
     </div>
   );
 }

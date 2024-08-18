eca772d86e13a2974345503d7ba8134932193a47
RamKishore <mramki1803@gmail.com>
2024-08-18 16:19:55 +0530

    mobile responsive

--- app/components/layout/Navbar.tsx
+++ app/components/layout/Navbar.tsx
@@ -1,25 +1,53 @@
-import Link from 'next/link';
-import Image from 'next/image';
-import ikea from '../icons/ikea.svg';
-import SearchBar from '../SearchBar';
-import truck from '../icons/truck.svg';
-import store from '../icons/store.svg';
-import profile from '../icons/profile.svg';
-import heart from '../icons/heart.svg';
-import cart from '../icons/cart.svg';
+import Link from "next/link";
+import Image from "next/image";
+import ikea from "../icons/ikea.svg";
+import SearchBar from "../SearchBar";
+import truck from "../icons/truck.svg";
+import store from "../icons/store.svg";
+import profile from "../icons/profile.svg";
+import heart from "../icons/heart.svg";
+import cart from "../icons/cart.svg";
 
 const Navbar = () => {
   return (
-    <nav className="text-black p-6 w-full h-[var(--navbar-height)] shadow-md">
-      <div className="max-w-full mx-auto flex justify-between items-center">
-        <Link href="/" className="text-xl font-bold"> <Image src={ikea} alt='ikea-icon'/></Link>
-        <SearchBar/>
-        <div className='flex gap-5 text-sm'>
-          <div className='flex gap-2 cursor-pointer'><Image src={truck} alt='truck-icon'/><span>Enter postal code</span></div>
-          <div className="mr-4 flex gap-2"><Image src={store} alt='store-icon'/><span>Select store</span></div>
-          <div className="mr-4 flex gap-2 cursor-pointer"><Image src={profile} alt='profile-icon'/><span>Hej! Log in</span></div>
-          <div><Image src={heart} alt='heart-icon'/></div>
-          <Link href={"/shoppingcart"}><Image src={cart} alt='cart-icon'/></Link>
+    <nav className="text-black p-4 md:p-6 h-[var(--navbar-height)] shadow-md flex flex-col md:flex-row md:items-center md:justify-between">
+      <div className="flex items-center justify-between w-full">
+        <Link href="/" className="text-xl font-bold flex items-center gap-2">
+          <Image src={ikea} alt="ikea-icon" width={50} height={50} />
+        </Link>
+
+        {/* Mobile Icons */}
+        <div className="md:hidden flex gap-4 items-center">
+          <Image src={truck} alt="truck-icon" width={24} height={24} />
+          <Image src={store} alt="store-icon" width={24} height={24} />
+          <Image src={profile} alt="profile-icon" width={24} height={24} />
+        </div>
+      </div>
+
+      <div className="hidden md:flex flex-grow justify-center mt-4 md:mt-0">
+        <SearchBar />
+      </div>
+
+      <div className="flex gap-4 items-center mt-4 md:mt-0">
+        <div className="hidden md:flex gap-2 items-center cursor-pointer">
+          <Image src={truck} alt="truck-icon" width={24} height={24} />
+          <span>Enter postal code</span>
+        </div>
+        <div className="hidden md:flex gap-2 items-center mr-4">
+          <Image src={store} alt="store-icon" width={24} height={24} />
+          <span>Select store</span>
+        </div>
+
+        <div className="flex gap-5 text-sm">
+          <div className="hidden md:flex gap-2 cursor-pointer">
+            <Image src={profile} alt="profile-icon" width={24} height={24} />
+          </div>
+          <div>
+            <Image src={heart} alt="heart-icon" width={24} height={24} />
+          </div>
+          <Link href={"/shoppingcart"}>
+            <Image src={cart} alt="cart-icon" width={24} height={24} />
+          </Link>
         </div>
       </div>
     </nav>

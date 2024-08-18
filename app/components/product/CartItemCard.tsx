994a7ac1d20bcd936929f587bb0d95da7ad04930
RamKishore <mramki1803@gmail.com>
2024-08-18 15:40:34 +0530

    cartPage, order summary

--- app/components/product/CartItemCard.tsx
+++ app/components/product/CartItemCard.tsx
@@ -3,8 +3,8 @@ import { useDispatch, useSelector } from "react-redux";
 import { addItem, removeItem, updateQuantity } from "../../redux/features/cart/cartSlice";
 import { RootState } from "../../redux/store";
 import Image from "next/image";
-import addtocart from "../icons/addtocart.svg";
 import deleteicon from "../icons/delete.svg";
+import { useToast } from "@/components/ui/use-toast"
 
 type CartItemCardProps = {
   id: number;
@@ -26,6 +26,8 @@ const CartItemCard = ({
   const dispatch = useDispatch();
   const cart = useSelector((state: RootState) => state.cart.items);
 
+  const { toast } = useToast()
+
   const handleIncrement = () => {
     dispatch(updateQuantity({ id, quantity: quantity + 1 }));
   };
@@ -35,6 +37,13 @@ const CartItemCard = ({
       dispatch(updateQuantity({ id, quantity: quantity - 1 }));
     }
   };
+
+  const handleRemoveItem = () => {
+    dispatch(removeItem(id));
+    toast({
+      title: `${name} removed from your shopping cart`,
+    });
+  }
 
   return (
     <div className="flex items-center p-4 border-b border-gray-200">
@@ -68,7 +77,7 @@ const CartItemCard = ({
               +
             </button>
           </div>
-          <div><Image src={deleteicon} alt="delete icon"/></div>
+          <div><Image src={deleteicon} alt="delete icon" onClick={handleRemoveItem}/></div>
         </div>
       </div>
     </div>
--- app/shoppingcart/page.tsx
+++ app/shoppingcart/page.tsx
@@ -3,16 +3,12 @@ import { useSelector } from "react-redux";
 import { RootState } from "../redux/store";
 import CartItemCard from "../components/product/CartItemCard";
 import { useEffect, useState } from "react";
+import Image from "next/image"; // Import Image component from next/image
+import emptybag from "../components/images/emptybag.png";
+import Link from "next/link";
 
 export default function CartPage() {
   const cart = useSelector((state: RootState) => state.cart.items);
-
-  useEffect(() => {
-    const calculateTotalPrice = () => {
-      return cart.reduce((acc, item) => acc + item.price * item.quantity, 0);
-    };
-    calculateTotalPrice();
-  }, [cart]);
 
   const calculateTotalPrice = () => {
     return cart.reduce((acc, item) => acc + item.price * item.quantity, 0);
@@ -34,68 +30,85 @@ export default function CartPage() {
   };
 
   return (
-    <div className="p-10 flex">
-      {/* Shopping bag section */}
-      <div className="w-[60vw]">
-        <h1 className="font-extrabold text-4xl mb-8">Shopping bag</h1>
-        {cart.map((item) => (
-          <CartItemCard
-            key={item.id}
-            id={item.id}
-            name={item.name}
-            price={item.price}
-            quantity={item.quantity}
-            dimensions={item.dimensions}
-            images={item.images}
-          />
-        ))}
-      </div>
-
-      {/* Order details Section */}
-      <div className="w-[40vw] p-6 bg-white shadow-md rounded-lg">
-        <h1 className="font-extrabold text-2xl mb-8 text-gray-900">
-          Order Summary
-        </h1>
-
-        <div className="flex items-center justify-between border-b border-gray-300 pb-4 mb-4">
-          <span className="text-lg font-semibold text-gray-800">Products:</span>
-          <span className="text-lg font-semibold text-gray-900">
-            ${calculateTotalPrice().toFixed(2)}
-          </span>
+    <div className="p-10 flex flex-col lg:flex-row">
+      {cart.length === 0 ? (
+        <div className="flex">
+          <div className="flex flex-col justify-center w-1/2">
+            <h1 className="font-extrabold text-4xl mb-8">
+              Your shopping bag is empty
+            </h1>
+            <p className="text-gray-600 mb-6">
+              Looks like you haven't added any items to your shopping bag yet.
+            </p>
+            <Link className="bg-black text-white rounded-xl p-6 text-center" href={'/search?q=2'}>Go back to shopping</Link>
+          </div>
+          <div className="w-[50vw]">
+          <Image src={emptybag} alt="Empty shopping bag" />
+          </div>
         </div>
-
-        {/* Apply Discount Code Section */}
-        <div className="mb-6">
-          <button
-            onClick={handleToggleDiscount}
-            className="text-blue-600 font-medium hover:underline focus:outline-none"
-          >
-            {showDiscount ? "Hide Discount Code" : "Apply Discount Code"}
-          </button>
-          {showDiscount && (
-            <div className="mt-4 flex items-center">
-              <input
-                type="text"
-                value={discountCode}
-                onChange={(e) => setDiscountCode(e.target.value)}
-                placeholder="Enter discount code"
-                className="border border-gray-300 rounded-l-md px-3 py-2 w-full"
+      ) : (
+        <>
+          <div className="w-full lg:w-3/5">
+            <h1 className="font-extrabold text-4xl mb-8">Shopping bag</h1>
+            {cart.map((item) => (
+              <CartItemCard
+                key={item.id}
+                id={item.id}
+                name={item.name}
+                price={item.price}
+                quantity={item.quantity}
+                dimensions={item.dimensions}
+                images={item.images}
               />
-              <button
-                onClick={handleApplyDiscount}
-                className="bg-blue-600 text-white px-4 py-2 rounded-r-md hover:bg-blue-700 transition-colors duration-300"
-              >
-                Apply
-              </button>
-            </div>
-          )}
-        </div>
+            ))}
+          </div>
 
-        {/* Continue to payment section */}
-        <button className="w-full py-3 bg-blue-600 text-white font-semibold rounded-lg hover:bg-blue-700 transition-colors duration-300">
-          Continue to Checkout
-        </button>
-      </div>
+          <div className="w-full lg:w-2/5 p-6 bg-white shadow-md rounded-lg mt-8 lg:mt-0">
+            <h1 className="font-extrabold text-2xl mb-8 text-gray-900">
+              Order Summary
+            </h1>
+
+            <div className="flex items-center justify-between border-b border-gray-300 pb-4 mb-4">
+              <span className="text-lg font-semibold text-gray-800">
+                Products:
+              </span>
+              <span className="text-lg font-semibold text-gray-900">
+                ${calculateTotalPrice().toFixed(2)}
+              </span>
+            </div>
+
+            <div className="mb-6">
+              <button
+                onClick={handleToggleDiscount}
+                className="text-blue-600 font-medium hover:underline focus:outline-none"
+              >
+                {showDiscount ? "Hide Discount Code" : "Apply Discount Code"}
+              </button>
+              {showDiscount && (
+                <div className="mt-4 flex items-center">
+                  <input
+                    type="text"
+                    value={discountCode}
+                    onChange={(e) => setDiscountCode(e.target.value)}
+                    placeholder="Enter discount code"
+                    className="border border-gray-300 rounded-l-md px-3 py-2 w-full"
+                  />
+                  <button
+                    onClick={handleApplyDiscount}
+                    className="bg-blue-600 text-white px-4 py-2 rounded-r-md hover:bg-blue-700 transition-colors duration-300"
+                  >
+                    Apply
+                  </button>
+                </div>
+              )}
+            </div>
+
+            <button className="w-full py-3 bg-blue-600 text-white font-semibold rounded-lg hover:bg-blue-700 transition-colors duration-300">
+              Continue to Checkout
+            </button>
+          </div>
+        </>
+      )}
     </div>
   );
 }
